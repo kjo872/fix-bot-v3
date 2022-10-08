04209f7cb49b5070633eba29f36b4edf9373850d
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-10-08 19:16:15 +0700

    Update creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -6,7 +6,7 @@ let name = await conn.getName(who)
 
 
 if (command == 'whmods1') {
- let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner WHMODS;Bot;;Md\nFN:Owner\nNICKNAME:👑 frmndaa\nORG:WH\nTITLE:MODS\nitem1.TEL;waid=6282365089294:+62 823-6508-9294\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://instagram.com/yxbxrock\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:firmanman117@gmail.com\nitem3.X-ABLabel:💌 Mail Owner Bot\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 KEPO LU YAH\nEND:VCARD`
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner WHMODS;Bot;;Md\nFN:Owner\nNICKNAME:👑 frmndaa\nORG:Owner Bot\nTITLE:MODS\nitem1.TEL;waid=6282365089294:+62 823-6508-9294\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://instagram.com/yxbxrock\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:firmanman117@gmail.com\nitem3.X-ABLabel:💌 Mail Owner Bot\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 KEPO LU YAH\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fakes })
 let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Nih Owner *${conn.user.name}* kak`
     await conn.sendButton(m.chat, caption, author, null, [['🎀 Sapa Owner', 'Huuu']], m, { quoted: tag_own, mentions: conn.parseMention(caption) })
